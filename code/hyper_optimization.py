f68943ad27bdb94f0ed1f9d5ee79039399330752
mbaumer <mbaumer12@gmail.com>
2016-02-13 17:22:54 -0800

    new class for hyperparam optimizing

---
+++ code/hyper_optimization.py
@@ -0,0 +1,182 @@
+from __future__ import division
+'''
+Transfer learning from VGG16
+See https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3 for more info
+'''
+import h5py
+import numpy as np
+import pandas as pd
+
+from keras import backend as K
+from keras.models import Sequential
+from keras.optimizers import SGD, Adam
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, ZeroPadding2D, MaxPooling2D
+from keras.callbacks import Callback
+
+from sklearn.cross_validation import train_test_split
+from keras.utils import np_utils, generic_utils
+
+import matplotlib
+matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
+import matplotlib.pyplot as plt
+
+
+weights_path = '/home/ubuntu/cs231n/data/vgg16_weights.h5'
+img_width, img_height = 128, 128
+
+# build the VGG16 network with our input_img as input
+first_layer = ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height))
+
+model = Sequential()
+model.add(first_layer)
+
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+# get the symbolic outputs of each "key" layer (we gave them unique names).
+layer_dict = dict([(layer.name, layer) for layer in model.layers])
+
+
+# Load the weights from our dropbox folder (about 0.5 GB worth) --------------------------
+f = h5py.File(weights_path)
+
+for k in range(f.attrs['nb_layers']):
+    if k >= len(model.layers):
+        break         # we don't look at the last (fully-connected) layers in the savefile
+    g = f['layer_{}'.format(k)]
+    weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+    model.layers[k].set_weights(weights)
+f.close()
+print('Model loaded.')
+
+
+for layer in model.layers:
+    layer.trainable = False
+
+# Add our own architecture --------------------------
+
+model.add(Flatten())
+# Note: Keras does automatic shape inference.
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+# model.add(Dense(256))
+# model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+
+model.add(Dense(3))
+model.add(Activation('softmax'))
+
+
+X = np.load('/home/ubuntu/cs231n/data/X.npy').astype('float32')
+y = np.load('/home/ubuntu/cs231n/data/Y.npy').astype('float32')
+X -= np.mean(X,axis=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+y_train, y_test = [np_utils.to_categorical(x) for x in (y_train, y_test)]
+
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
+class CrossValidator(object):
+
+	def __init__(self):
+		self.batch_histories = []
+		self.epoch_histories = []
+		self.best_model = None
+		self.best_val_loss = 1e9
+
+	def run(self,n_trials):
+		for i in range(n_trials):
+			learning_rate = np.random.uniform(-5,-2,1)
+			print 'running crossval trial', i, 'learning rate is', learning_rate
+			adam = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+			model.compile(loss='categorical_crossentropy', optimizer=adam)
+
+			print "Model has compiled."
+			# model.compile(loss='categorical_crossentropy', optimizer=Adam)
+			batch_history = LossHistory()
+			epoch_history = model.fit(X_train, y_train, batch_size=32, nb_epoch=10, verbose=1,callbacks=[batch_history], validation_split=0.2)
+		    if i == 0: 
+		    self.best_model = model
+		    	self.best_val_loss = epoch_history.history['val_loss'][-1]
+		    else:
+		    	if epoch_history.history['val_loss'][-1] < self.best_val_loss:
+		    		self.best_model = model
+		    		self.best_val_loss = epoch_history.history['val_loss'][-1]
+
+		    self.batch_histories.append(batch_history)
+		    self.epoch_histories.append(epoch_history)
+
+			# print "Train Accuracy"
+			# train_predictions = model.predict(X_train, batch_size=32, verbose=1)
+			# print np.sum(np.argmax(train_predictions,axis=1) == np.argmax(y_train,axis=1))/X_train.shape[0]
+
+			# print "Val Accuracy"
+			# val_predictions = model.predict(X_val,batch_size=32,verbose=1)
+			# print np.sum(np.argmax(val_predictions,axis=1) == np.argmax(y_val,axis=1))/X_val.shape[0]
+
+	def plot(self):
+		fig1,ax1 = plt.figure()
+		ax1.set_xlabel('Batch Number')
+		ax1.set_ylabel('Training Loss')
+		for history in self.batch_histories:
+			plt.plot(history)
+		plt.savefig('training_losses.png')
+
+		fig2,ax2 = plt.figure()
+		ax2.set_xlabel('Epoch Number')
+		ax2.set_ylabel('Validation Loss')
+		for history in self.epoch_histories:
+			plt.plot(history)
+		plt.savefig('validation_losses.png')
+
+solver = CrossValidator()
+solver.run(5)
+train_predictions = solver.best_model.predict(X_train, batch_size=32, verbose=1)
+print 'best model has learning rate of', solver.best_model.lr
+print 'training accuracy is', np.sum(np.argmax(train_predictions,axis=1) == np.argmax(y_train,axis=1))/X_train.shape[0]
+val_predictions = solver.best_model.predict(X_val,batch_size=32,verbose=1)
+print 'validation accuracy is', np.sum(np.argmax(val_predictions,axis=1) == np.argmax(y_val,axis=1))/X_val.shape[0]
+test_predictions = solver.best_model.predict(X_test,batch_size=32,verbose=1)
+print 'test accuracy is', np.sum(np.argmax(test_predictions,axis=1) == np.argmax(y_test,axis=1))/X_test.shape[0]
+solver.plot()
+print 'find loss curves in current directory: training_losses.png and validation_losses.png'
+
